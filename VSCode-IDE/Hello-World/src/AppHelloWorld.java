cd7fc4caa2f157608f56bc344b737f45fa6f48dc
Nando <fsnando.012@outlook.com>
2025-04-27 12:57:01 -0300

    Add initial Java project setup and Hello World program
    Set up a basic Java project structure with a "Hello World" program and arithmetic operations in the `AppHelloWorld.java` file. Include a `README.md` with setup instructions, `.vscode/settings.json` for VS Code configuration, and the IntelliJ module file `VSCode-IDE.iml`.

---
+++ VSCode-IDE/Hello-World/src/AppHelloWorld.java
@@ -0,0 +1,21 @@
+public class AppHelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        System.out.println("Soma de A com B: " + sum);
+
+        int sub = a - b;
+        System.out.println("Subtração de A com B: " + sub);
+
+        int mult = a * b;
+        System.out.println("Multiplicação de A com B: " + mult);
+
+        int div = a / b;
+        System.out.println("Divisão de A com B: " + div);
+        
+    }
+}
